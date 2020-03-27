a71477538b68b848a25cb48887dca330f244165e
ZHANG JINSHAN <zhangjs2233@gmail.com>
2020-03-27 15:59:27 +0800

    changed

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,30 +1,29 @@
-
-const assert=require('assert')
+const assert = require('assert')
 class ApplicationError extends Error {
-    constructor(message, options = {}) {
-      assert(typeof message === 'string');
-      assert(typeof options === 'object');
-      assert(options !== null);
-      super(message);
-  
-      // Attach relevant information to the error instance
-      // (e.g., the username).
-      for (const [key, value] of Object.entries(options)) {
-        this[key] = value;
-      }
-    }
-  
-    get name() {
-      return this.constructor.name
+  constructor(message, options = {}) {
+    assert(typeof message === 'string');
+    assert(typeof options === 'object');
+    assert(options !== null);
+    super(message);
+
+    // Attach relevant information to the error instance
+    // (e.g., the username).
+    for (const [key, value] of Object.entries(options)) {
+      this[key] = value;
     }
   }
 
-  class UserFacingError extends ApplicationError {
-	constructor(message, options = {}) {
-		super(message, options);
-	}
+  get name() {
+    return this.constructor.name
+  }
 }
 
-module.exports={
-    UserFacingError
+class UserFacingError extends ApplicationError {
+  constructor(message, options = {}) {
+    super(message, options);
+  }
+}
+
+module.exports = {
+  UserFacingError
 }
